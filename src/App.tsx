22e40010329d00c3bb95504bf66cd15c7c45b13c
vedantbhatotia <vedantbhatotia@gmail.com>
2024-10-12 12:38:26 +0530

    zod with localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,55 @@
-import {Person} from './person';
-import {useState} from 'react';
-function App() {
-  return (
-    // <div>
-    //   <Person name="John" age={25} isMarried={true}/>
-    // </div>
+import { z } from "zod";
+// import {Person} from './person';
+// import {useState} from 'react';
+// function App() {
+//   return (
+//     // <div>
+//     //   <Person name="John" age={25} isMarried={true}/>
+//     // </div>
+//     <div>
+//       hfe
+//     </div>
+//   )
+// }
+
+// export default App
+// const CartSchema = z.object({
+//   totalPrice:z.number(),
+//   items:z.array(z.object({
+//     id:z.string(),
+//     name:z.string(),
+//     price:z.number(),
+//     quantity:z.number()
+//   }))
+// })
+const CartSchema = z.array(
+  z.object({
+    id:z.string(),
+    name:z.string(),
+    price:z.number(),
+    quantity:z.number().positive().int()
+  })
+)
+function App(){
+  return(
+    <Cart></Cart>
+  )
+}
+export default App;
+
+function Cart(){
+  const Cart:unknown = JSON.parse(localStorage.getItem("cart") || "{}");
+  const validatedCart = CartSchema.safeParse(Cart);
+  if(!validatedCart.success){
+    localStorage.removeItem("cart");
+    return;
+  }else{
+    console.log(validatedCart.data);
+  }
+
+  return(
     <div>
-      hfe
+      Cart
     </div>
   )
 }
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,22 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import App from './App'
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-import z from 'zod';
-const UserSchema = z.object({
-  name:z.string(),
-  age:z.number(),
-  email:z.string().email()
-})
-const user = {
-  name:"Vedant",
-  age:12,
-  email:"vedant@gmail.com"
-}
-const user1 = UserSchema.parse(user);
-console.log("hwllo"+user1);
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+// import z from 'zod';
+// const UserSchema = z.object({
+//   name:z.string(),
+//   age:z.number(),
+//   email:z.string().email()
+// })
+// type User = z.infer<typeof UserSchema>;
+// const user:User = {
+//   name:"Vedant",
+//   age:12,
+//   email:"vedant@gmail.com"
+// }
+// const user1 = UserSchema.parse(user);
+// console.log("hwllo"+user1);
